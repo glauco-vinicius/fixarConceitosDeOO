8d9db3133ab27f6405cc8b3acffb4f93af567c33
GLAUCO VINICIUS SCHEFFEL <glauco.scheffel@catolicasc.org.br>
2023-05-05 21:55:06 -0300

    Initial commit

---
+++ src/main/java/org/catolicasc/educational/Aluno.java
@@ -0,0 +1,30 @@
+package org.catolicasc.educational;
+
+public class Aluno extends Pessoa {
+
+    private String matricula;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Aluno aluno)) return false;
+        if (!super.equals(o)) return false;
+
+        return getMatricula() != null ? getMatricula().equals(aluno.getMatricula()) : aluno.getMatricula() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (getMatricula() != null ? getMatricula().hashCode() : 0);
+        return result;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
---
+++ src/main/java/org/catolicasc/educational/Disciplina.java
@@ -0,0 +1,32 @@
+package org.catolicasc.educational;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Disciplina {
+    private String nome;
+    public Disciplina(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Disciplina{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Disciplina that)) return false;
+
+        return Objects.equals(nome, that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return nome != null ? nome.hashCode() : 0;
+    }
+}
